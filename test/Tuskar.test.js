82167499167c621dbc6ae8e6929594f5c6ffff0e
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2024-04-22 21:08:25 +0530

    modified test case

--- test/Tuskar.test.js
+++ test/Tuskar.test.js
@@ -5,6 +5,6 @@ contract("Tuskar", (accounts) => {
     it("Should credit NFT to an Specific Account", async () => {
         const tuskarInstance = await Tuskar.deployed();
         await tuskarInstance.safeMint(accounts[1], "tuskar_1.json");
-        assert.equal((await tuskarInstance.ownerOf(0), accounts[1], "Owner of token 1 is not equal to account 2"));
+        assert.equal(await tuskarInstance.ownerOf(0), accounts[1], "Owner of token 1 is not equal to account 2");
     })
 })
